d6b12a4dcdb2c791b73923d454a47fd799eb9972
code-of-sugar <kennbi.ryo3233@gmail.com>
2019-12-09 23:33:29 +0900

    chatbotメッセージ表示機能の追加

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -6,12 +6,41 @@ $(function(){
                           `
     return userMessageHTML
   } 
-  $('#js-btn').on('click',function(){
-    var userMessage = $('#js-message').val();
-    
+
+  function chatbotMessageBuild(message){
+    var chatbotMessageHTML= ` <div class="balloon6">
+                                <div class="chatting">
+                                  <div class="says">
+                                    <p>${message}</p>
+                                  </div>
+                                </div>
+                              </div>
+                            `
+    return chatbotMessageHTML
+  }
+
+  function ajaxGetMessage(userMessage){
+    $.ajax({
+      url: "/",
+      type: 'post',
+      data: JSON.stringify(userMessage),
+      contentType:'application/json',
+      success: function(data){
+        $('.main-contents').append(chatbotMessageBuild(data))
+      }
+    })
+  }
+
+  function sendMyMessage(userMessage){    
     if (userMessage != ""){
       var HTML = userMessageBuild(userMessage)
       $('.main-contents').append(HTML)
     }
+  }
+  $('#js-btn').on('click',function(){
+    var userMessage = $('#js-message').val();
+    sendMyMessage(userMessage)
+    ajaxGetMessage(userMessage)
   })
+  
 })
